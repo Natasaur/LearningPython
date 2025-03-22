8378f8f61c43a8e418ee1c709c74b6fd1187294c
Natasaur <n.garcia.s787@edu.utc.mx>
2025-03-21 21:42:33 -0600

    Primeras notas del curso Ultimate Python Hola Mundo

---
+++ 07-numeros-funciones.py
@@ -0,0 +1,10 @@
+import math
+
+print(round(1.3))
+print(abs(-77))
+
+print(math.ceil(1.1)) # Número entero siguiente
+print(math.floor(1.99999)) # Número entero más cercano hacia abajo
+print(math.isnan(23)) # Verífica su el argumento es un número
+print(math.pow(10, 3)) # Potencia
+print(math.sqrt(27)) # Raíz cuadrada
---
+++ 08-calculadora.py
@@ -0,0 +1,18 @@
+num1 = input("Ingresa un número: ")
+num2 = input("Ingresa otro número: ")
+
+num1 = int(num1)
+num2 = int(num2)
+
+suma = num1 + num2
+resta = num1 - num2
+multi = num1 * num2
+divi = num1 / num2
+
+mensaje = """
+Para los números {num1} y {num2},
+la suma es: {suma},
+la resta es: {resta},
+la multiplicación es: {multi},
+la división es: {divi}
+"""
